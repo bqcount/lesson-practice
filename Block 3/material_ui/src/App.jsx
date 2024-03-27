8b711c37f837cc056a1e4b1f88cd24ad65f475dc
Diego Fernandez <judifede217@gmail.com>
2024-03-27 12:40:57 +0000

    Login material UI

---
+++ Block 3/material_ui/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import LoginForm from '../src/components/LoginForm.jsx'
+
+function App() {
+
+  return (
+    <>
+      <LoginForm />
+    </>
+  )
+}
+
+export default App
---
+++ Block 3/material_ui/src/components/LoginForm.jsx
@@ -0,0 +1,98 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardActions,
+  Typography,
+  TextField,
+  Button,
+  InputAdornment,
+  Icon,
+  IconButton,
+} from "@mui/material"
+
+import {
+  LockOutlined,
+  EmailOutlined,
+  VisibilityOffOutlined,
+  VisibilityOutlined,
+} from "@mui/icons-material"
+import { grey } from "@mui/material/colors"
+import { useState } from "react"
+
+function LoginForm() {
+  const [isPassVisible, setIsPassVisible] = useState(false)
+
+  return (
+    <Card sx={{ maxWidth: "700px", backgroundColor: grey[300] }} raised={true}>
+      <CardHeader title="Login" />
+      <CardContent>
+        <TextField
+          label="Email"
+          //   variant="filled"
+          // variant="outlined" predeterminada
+          margin="normal"
+          type="email"
+          fullWidth={true}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment>
+                <Icon >
+                  <EmailOutlined />
+                </Icon>
+              </InputAdornment>
+            ),
+          }}
+        ></TextField>
+        <TextField
+          label="Password"
+          variant="outlined"
+          type={isPassVisible ? "text" : "password"}
+          margin="normal"
+          fullWidth={true}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment>
+                <Icon>
+                  <LockOutlined />
+                </Icon>
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment>
+                <IconButton
+                  onClick={() => {
+                    setIsPassVisible((oldState) => !oldState)
+                  }}
+                >
+                  {isPassVisible ? (
+                    <VisibilityOffOutlined />
+                  ) : (
+                    <VisibilityOutlined />
+                  )}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        ></TextField>
+        {/* <Typography component="div" variant="h1">
+          Mi tarjeta
+        </Typography>
+        <Typography variant="h1">Mi tarjeta</Typography>
+        <Typography component="span">Mi tarjeta</Typography> */}
+      </CardContent>
+      <CardActions
+        sx={{ display: "flex", justifyContent: "end", padding: "15px" }}
+      >
+        <Button size="small" color="secondary" variant="contained">
+          Register
+        </Button>
+        <Button size="small" color="primary" variant="contained">
+          Login
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default LoginForm
